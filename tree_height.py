e57615995b7b0d351e5847bc18ea2db183ebeb2a
221RDB178 <117149941+221RDB187@users.noreply.github.com>
2023-02-20 16:24:59 +0000

    try1

--- tree_height.py
+++ tree_height.py
@@ -1,19 +1,70 @@
 # python3
-
+import os
 import sys
 import threading
 
+def input_read():
+    source = input().lower()
+    if source == 'i':
+        print("Enter nr. of nodes")
+        n = int(input())
+        print("Parents of nodes")
+        parents = list(map(int, input().split()))
+        return n, parents
+    elif source == 'f':
+        while True:
+            print("file name")
+            filename = input()
+            if 'a' in filename:
+                print("Cant contain 'a'")
+            elif not os.path.isfile('inputs/' + filename):
+                print("File not found")
+            else:
+                with open('inputs/' + filename, "r") as file:
+                    n = int(file.readlines().strip())
+                    parents = list(map(int, file.readline().strip().split()))
+                    return n, parents
+    else:
+        print("Invalid")
+        return input_read()
 
-def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+def build_tree(n, parents):
+    # Initialize an empty adjacency list for each node
+    adj = [[] for _ in range(n)]
+
+    # Build the adjacency list
+    for i in range(n):
+        if parents[i] == -1:
+            root = i
+        else:
+            adj[parents[i]].append(i)
+
+    return root, adj
+
+print("Input")
+def height(root, adj):
+    if not adj[root]:
+        return 1
+    else:
+        max_child_height = 0
+        for child in adj[root]:
+            child_height = height(child, adj)
+            if child_height > max_child_height:
+                max_child_height = child_height
+        return max_child_height + 1
 
 
-def main():
+n = int(input())
+parents = list(map(int, input().split()))
+
+root, adj = build_tree(n, parents)
+tree_height = height(root, adj)
+
+print("Output:")
+print(tree_height)
+
     # implement input form keyboard and from files
-    pass
+    
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
     
@@ -27,4 +78,4 @@ def main():
 # of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+threading.Thread(target=height).start()
