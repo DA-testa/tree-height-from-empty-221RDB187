ed5a3ad3c705826bc592df4d1423eec17cc2f8dc
221RDB178 <117149941+221RDB187@users.noreply.github.com>
2023-02-23 15:48:36 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -39,12 +39,11 @@ def main():
             n = int(f.readline().strip())
             arr = list(map(int, f.readline().strip().split()))
             print(compute_height(n, arr))
-    elif input_type == 'I':
+    else:
         n = int(input("Enter the number of nodes: ").strip())
         arr = list(map(int, input("Enter the parent of each node separated by space: ").strip().split()))
         print(compute_height(n, arr))
-    else:
-        return
+
 
 
 # In Python, the default limit on recursion depth is rather low,
