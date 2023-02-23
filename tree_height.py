8560807bd7a1557280372ec653efc8bf9f8c0ff8
221RDB178 <117149941+221RDB187@users.noreply.github.com>
2023-02-23 15:38:24 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -37,8 +37,8 @@ def main():
             n = int(f.readline().strip())
             arr = list(map(int, f.readline().strip().split()))
             print(compute_height(n, arr))
-    elif input_type == 'I':
-        n = int(input("Enter the number of nodes: ").replace('\r', ''))
+    else:
+        n = int(input("Enter the number of nodes: ").strip())
         arr = list(map(int, input("Enter the parent of each node separated by space: ").strip().split()))
         print(compute_height(n, arr))
 
